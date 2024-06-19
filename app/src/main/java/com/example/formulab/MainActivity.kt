19d8f0587c569cce548e749a6d21b0784f100502
Eduardo <edureisfaria@gmail.com>
2024-06-20 00:13:48 +0100

    Added Camera Functionality to Fragment
    Updated CameraFragment.kt
    Added ic_camera.xml
    Updated fragment_camera.xml
    Updated MainActivity.kt for permissions

--- app/src/main/java/com/example/formulab/MainActivity.kt
+++ app/src/main/java/com/example/formulab/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.formulab
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
@@ -10,27 +16,151 @@ import androidx.navigation.ui.NavigationUI
 import com.example.formulab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var view : ActivityMainBinding
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-//        setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-//    }
+    private lateinit var view: ActivityMainBinding
+
+    //State of Permissions
+    private var hasCameraPermission = false
+    private var hasStoragePermission = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         view = ActivityMainBinding.inflate(layoutInflater)
         setContentView(view.root)
+
+        checkPermissions()
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
-        finish()
+    private fun checkPermissions() {
+        //Check initial permissions
+        for (permission in REQUIRED_PERMISSIONS) {
+            when (permission) {
+                //TODO:
+                //For future permissions duplicate this snippet and change type of permission and corresponded val
+                Manifest.permission.CAMERA -> {
+                    hasCameraPermission =
+                        ContextCompat.checkSelfPermission(
+                            this,
+                            permission
+                        ) == PackageManager.PERMISSION_GRANTED
+                }
+
+            }
+        }
+
+
+
+        ActivityCompat.requestPermissions(
+            this,
+            REQUIRED_PERMISSIONS.toTypedArray(),
+            REQUIRED_PERMISSIONS.map { PERMISSION_REQUEST_CODES[it]!! }.maxOrNull()!!
+        )
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            CAMERA_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    hasCameraPermission = true
+                    //updateUI()
+                }
+            }
+
+            STORAGE_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    hasStoragePermission = true
+                    //updateUI()
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val CAMERA_PERMISSION_REQUEST_CODE = 1001
+        private const val STORAGE_PERMISSION_REQUEST_CODE = 1002
+
+        private val REQUIRED_PERMISSIONS = mutableListOf<String>().apply {
+            add(Manifest.permission.CAMERA)
+            // Storage permissions based on Android version
+            when {
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> {
+                    // Android 13 and later
+                    addAll(
+                        listOf(
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.MANAGE_EXTERNAL_STORAGE,
+                            Manifest.permission.ACCESS_MEDIA_LOCATION,
+                            Manifest.permission.READ_MEDIA_IMAGES
+                        )
+                    )
+                }
+
+                Build.VERSION.SDK_INT in Build.VERSION_CODES.Q..Build.VERSION_CODES.S_V2 -> {
+                    // Android 10 to Android 12
+                    addAll(
+                        listOf(
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.MANAGE_EXTERNAL_STORAGE,
+                            Manifest.permission.ACCESS_MEDIA_LOCATION
+                        )
+                    )
+                }
+
+                else -> {
+                    // Prior to Android 10
+                    addAll(
+                        listOf(
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        )
+                    )
+                }
+            }
+        }
+
+        //Map of Common request codes
+        private val COMMON_PERMISSION_REQUEST_CODES = mapOf(
+            Manifest.permission.CAMERA to CAMERA_PERMISSION_REQUEST_CODE
+        )
+
+        //Map of Storage request codes
+        private val STORAGE_PERMISSION_REQUEST_CODES = when {
+            //Storage Request codes based of Android Version
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> mapOf(
+                // Android 13 and later
+                Manifest.permission.WRITE_EXTERNAL_STORAGE to STORAGE_PERMISSION_REQUEST_CODE,
+                Manifest.permission.READ_EXTERNAL_STORAGE to STORAGE_PERMISSION_REQUEST_CODE,
+                Manifest.permission.MANAGE_EXTERNAL_STORAGE to STORAGE_PERMISSION_REQUEST_CODE,
+                Manifest.permission.ACCESS_MEDIA_LOCATION to STORAGE_PERMISSION_REQUEST_CODE,
+                Manifest.permission.READ_MEDIA_IMAGES to STORAGE_PERMISSION_REQUEST_CODE
+            )
+
+            Build.VERSION.SDK_INT in Build.VERSION_CODES.Q..Build.VERSION_CODES.S -> mapOf(
+                // Android 10 to Android 12
+                Manifest.permission.WRITE_EXTERNAL_STORAGE to STORAGE_PERMISSION_REQUEST_CODE,
+                Manifest.permission.READ_EXTERNAL_STORAGE to STORAGE_PERMISSION_REQUEST_CODE,
+                Manifest.permission.MANAGE_EXTERNAL_STORAGE to STORAGE_PERMISSION_REQUEST_CODE,
+                Manifest.permission.ACCESS_MEDIA_LOCATION to STORAGE_PERMISSION_REQUEST_CODE
+            )
+
+            else -> mapOf(
+                // Prior to Android 10
+                Manifest.permission.WRITE_EXTERNAL_STORAGE to STORAGE_PERMISSION_REQUEST_CODE,
+                Manifest.permission.READ_EXTERNAL_STORAGE to STORAGE_PERMISSION_REQUEST_CODE
+            )
+        }
+
+        //Bundle Permission request codes
+        private val PERMISSION_REQUEST_CODES = mutableMapOf<String, Int>().apply {
+            putAll(COMMON_PERMISSION_REQUEST_CODES)
+            putAll(STORAGE_PERMISSION_REQUEST_CODES)
+        }
+
     }
 }
--- app/src/main/java/com/example/formulab/fragments/CameraFragment.kt
+++ app/src/main/java/com/example/formulab/fragments/CameraFragment.kt
@@ -5,56 +5,102 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import com.example.formulab.R
+import com.example.formulab.databinding.FragmentCameraBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CameraFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CameraFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    //Binding
+    private var _view: FragmentCameraBinding? = null
+    private val view get() = _view!!
+
+    private lateinit var cameraExecutor: ExecutorService
+    private var isCameraReady = false
+    private var imageCapture: ImageCapture? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        // Initialize camera executor
+        cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false)
+    ): View {
+        // Inflate the layout for this fragment using view binding
+        _view = FragmentCameraBinding.inflate(inflater, container, false)
+        return view.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Start camera preview after the view has been created
+        startCamera()
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(view.cameraPreview.surfaceProvider)
+                }
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    preview
+                )
+                // Set flag indicating that camera is ready
+                isCameraReady = true
+                updatePreviewVisibility()
+            } catch (exc: Exception) {
+                // Handle exceptions
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun updatePreviewVisibility() {
+        if (isCameraReady) {
+            // Fade in animation
+            val fadeInAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.fade_in)
+            view.cameraPreview.apply {
+                //TO FIX: Applying animation
+                startAnimation(fadeInAnimation)
+                visibility = View.VISIBLE
+            }
+        } else {
+            view.cameraPreview.visibility = View.INVISIBLE
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CameraFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CameraFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        private const val TAG = "Formula B"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 }
