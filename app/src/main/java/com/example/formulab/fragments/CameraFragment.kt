17cfa2d8012e243cf6bfe026606d3968ef202d2d
Eduardo <edureisfaria@gmail.com>
2024-06-21 02:39:32 +0100

    Adapted CameraFragment.kt to use ImageClassifierHelper

--- app/src/main/java/com/example/formulab/fragments/CameraFragment.kt
+++ app/src/main/java/com/example/formulab/fragments/CameraFragment.kt
@@ -11,24 +11,31 @@ import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.activityViewModels
+import com.example.formulab.ImageClassifierHelper
+import com.example.formulab.MainViewModel
 import com.example.formulab.R
 import com.example.formulab.databinding.FragmentCameraBinding
+import com.google.mediapipe.tasks.vision.core.RunningMode
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
-class CameraFragment : Fragment() {
+class CameraFragment : Fragment(), ImageClassifierHelper.ClassifierListener {
     //Binding
-    private var _view: FragmentCameraBinding? = null
-    private val view get() = _view!!
+    private var _FragmentCameraBinding: FragmentCameraBinding? = null
+    private val fragmentCameraBinding get() = _FragmentCameraBinding!!
 
-    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var backgroundExecutor: ExecutorService
     private var isCameraReady = false
     private var imageCapture: ImageCapture? = null
 
+    private val viewModel: MainViewModel by activityViewModels()
+    private lateinit var imageClassifierHelper: ImageClassifierHelper
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Initialize camera executor
-        cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
     override fun onCreateView(
@@ -36,14 +43,43 @@ class CameraFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment using view binding
-        _view = FragmentCameraBinding.inflate(inflater, container, false)
-        return view.root
+        _FragmentCameraBinding = FragmentCameraBinding.inflate(inflater, container, false)
+        return fragmentCameraBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        backgroundExecutor = Executors.newSingleThreadExecutor()
         // Start camera preview after the view has been created
-        startCamera()
+        backgroundExecutor.execute {
+
+            imageClassifierHelper = ImageClassifierHelper(
+                context = requireContext(),
+                runningMode = RunningMode.LIVE_STREAM,
+                threshold = viewModel.currentThreshold,
+                currentDelegate = viewModel.currentDelegate,
+                currentModel = viewModel.currentModel,
+                maxResults = viewModel.currentMaxResults,
+                imageClassifierListener = this
+            )
+
+            fragmentCameraBinding.cameraPreview.post {
+                // Set up the camera and its use cases
+                startCamera()
+            }
+        }
+    }
+
+    override fun onPause() {
+        // save ImageClassifier settings
+        viewModel.setModel(imageClassifierHelper.currentModel)
+        viewModel.setDelegate(imageClassifierHelper.currentDelegate)
+        viewModel.setThreshold(imageClassifierHelper.threshold)
+        viewModel.setMaxResults(imageClassifierHelper.maxResults)
+        super.onPause()
+
+        // Close the image classifier and release resources
+        //backgroundExecutor.execute { imageClassifierHelper.clearImageClassifier() }
     }
 
     private fun startCamera() {
@@ -56,7 +92,7 @@ class CameraFragment : Fragment() {
             val preview = Preview.Builder()
                 .build()
                 .also {
-                    it.setSurfaceProvider(view.cameraPreview.surfaceProvider)
+                    it.setSurfaceProvider(fragmentCameraBinding.cameraPreview.surfaceProvider)
                 }
 
             // Select back camera as a default
@@ -84,13 +120,13 @@ class CameraFragment : Fragment() {
         if (isCameraReady) {
             // Fade in animation
             val fadeInAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.fade_in)
-            view.cameraPreview.apply {
+            fragmentCameraBinding.cameraPreview.apply {
                 //TO FIX: Applying animation
                 startAnimation(fadeInAnimation)
                 visibility = View.VISIBLE
             }
         } else {
-            view.cameraPreview.visibility = View.INVISIBLE
+            fragmentCameraBinding.cameraPreview.visibility = View.INVISIBLE
         }
     }
 
@@ -99,8 +135,21 @@ class CameraFragment : Fragment() {
         private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        cameraExecutor.shutdown()
+    override fun onDestroyView() {
+        _FragmentCameraBinding = null
+        super.onDestroyView()
+        // Shut down our background executor
+        backgroundExecutor.shutdown()
+        backgroundExecutor.awaitTermination(
+            Long.MAX_VALUE, TimeUnit.NANOSECONDS
+        )
+    }
+
+    override fun onError(error: String, errorCode: Int) {
+        //TODO("Not yet implemented")
+    }
+
+    override fun onResults(resultBundle: ImageClassifierHelper.ResultBundle) {
+        //TODO("Not yet implemented")
     }
 }
